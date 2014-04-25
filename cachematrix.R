682e05364dc77feccd77a65bd17343a08bcbbffd
Andrew Maner <aom336@gmail.com>
2014-04-24 20:27:12 -0400

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below, together, allow the user to solve the inverse
+## of a square, invertable matrix, and to cache the inverse for
+## later access.  For example: pass a matrix to the makeCacheMatrix
+## function for the first time, and store the result in a variable.
+## Pass that variable to cacheSolve for the first time and the 
+## inverse will be generated and cached.  This may be a lengthy
+## process.  Subsequent calls of cacheSolve on that variable, however,
+## will yield the inverse in (potentially) substantially less time.
 
-## Write a short comment describing this function
+## This function takes a square, invertable matrix as an argument
+## and returns four functions (set, get, setsolve, & getsolve)
+## and stores them in the variable you assign 
+## (e.g., output <- makeCacheMatrix(<your matrix here>))
+## The matrix itself is stored in the parent environment.
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
@@ -18,10 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function takes as its argument the special variable created
+## in the previous function.  For example, if you stored your output 
+## in the variable output, you would pass that to cacheSolve
+## cacheSolve computes the inverse of the original matrix the first time
+## around, and caches it.  It retrieves the cached matrix upon subsequent
+## calss
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   s <- x$getsolve()
   if (!is.null(s)){
     message("getting cached data")
